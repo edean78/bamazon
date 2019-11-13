a963456dccf725a952b6f801141ce139eb45683f
edean78 <edean78@gmail.com>
2019-11-12 23:05:11 -0500

    Added a cli table and setTimeouts to start and restart the program

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 // Require mysql and inquirer
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table");
 
 // Create DB
 var myDB = {
@@ -25,9 +26,27 @@ function selectAll() {
     connection.query("select * from products;", (err, response) => {
         if (err) throw err;
 
-        console.log(response);
-        purchaseItem();
+        // instantiate
+        var table = new Table({
+            head: ['Item_Id', 'Product_Name', 'Department', 'Price', 'Stock_Quantity'],
+            colWidths: [15, 30, 30, 10, 15]
+        });
+
+        // table is an Array, so you can `push`, `unshift`, `splice` and friends
+        for(var i = 0; i < response.length; i++){
+            var data = response[i];
+        table.push([
+            data.item_id, data.product_name, data.department_name, data.price, data.stock_quantity
+        ]);
+        }
+        console.log(table.toString());
+
+        setTimeout(purchaseItem, 3000);
     });
+}
+
+function runAgain() {
+    setTimeout(selectAll, 3000);
 }
 
 function purchaseItem() {
@@ -53,18 +72,16 @@ function purchaseItem() {
             var quantity = answer.quantity;
             purchaseOrder(item, quantity);
 
-            console.log(item);
-            console.log(quantity);
         });
 }
 
 function purchaseOrder(id, quan) {
     connection.query('Select * FROM products WHERE item_id = ' + id + ';', function (err, res) {
-        if(err) throw err;
+        if (err) throw err;
 
         var products = res[0];
 
-        if(quan <= products.stock_quantity) {
+        if (quan <= products.stock_quantity) {
             var total = products.price * quan;
             var newQuan = products.stock_quantity - quan;
 
@@ -73,9 +90,12 @@ function purchaseOrder(id, quan) {
             console.log('Thank you for shopping with Bamazon!!!');
 
             connection.query('UPDATE products SET stock_quantity = ' + newQuan + ' WHERE item_id = ' + id + ';');
+
+            runAgain();
         } else {
             console.log("Sorry!!! Your order couldn't be processed due to insufficient stock\nPlease modify your order or check back at a later date.");
-            setTimeout(purchaseItem(), 3000);
+            runAgain();
         }
     });
 }
+
